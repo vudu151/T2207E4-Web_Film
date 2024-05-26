6ac5b04c276cb4736ff236bd742654ef6aeb20cd
NCanhhieu <109597988+NCanhhieu@users.noreply.github.com>
2024-05-26 16:56:49 +0700

    Procedure

--- proceduredoistatuspayment.sql
+++ proceduredoistatuspayment.sql
@@ -1,16 +1,19 @@
-DROP PROCEDURE IF EXISTS UpdatePaymentStatus;
+DROP PROCEDURE IF EXISTS UpdatePaymentAndAccountStatus;
 
-CREATE PROCEDURE UpdatePaymentStatus(
-    IN accountID
-)
+CREATE PROCEDURE UpdatePaymentAndAccountStatus()
 BEGIN
     DECLARE currentDate DATE;
-    -- ngay hien tai + them 1 thang
-    SET currentDate = DATE_ADD(CURRENT_DATE(), INTERVAL -1 MONTH);
+ 
+    SET currentDate = CURRENT_DATE();
     
-    UPDATE payments p 
-    WHERE p.account_id = accountID
-    SET status = 0
-    WHERE status = 1
-        AND DATE_ADD(created_at, INTERVAL 1 MONTH) <= currentDate;
+    START TRANSACTION;
+    
+    UPDATE payments p
+    INNER JOIN accounts a ON p.account_id = a.id
+    SET p.status = 0, a.level = 1
+    WHERE p.status = 1
+        AND DATE_ADD(p.created_at, INTERVAL 1 MONTH) <= currentDate
+        AND a.level = 2;
+    
+    COMMIT;
 END;
