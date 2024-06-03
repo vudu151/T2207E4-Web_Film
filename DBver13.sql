a8a09dc1c6223f82a21db387acb3880287cdaf41
NCanhhieu <109597988+NCanhhieu@users.noreply.github.com>
2024-06-03 19:46:09 +0700

    uptest

--- DBver13.sql
+++ DBver13.sql
@@ -4,22 +4,25 @@ CREATE TABLE `movies` (
 	`total_episode` INT,
 	`poster` VARCHAR(255),
 	`release` DATE,
-	`run_time` INT,
+	`total_duration` INT,
 	`mmpa_rating` VARCHAR(255),
 	`keywords` VARCHAR(255),
 	`description` TEXT(65535),
-	`view` INT,
+	`totalview` INT,
 	`level` SMALLINT,
-	`status` SMALLINT DEFAULT 1,
-	`categories_movies_id` BINARY(16),
+	`distributors_id` BINARY(16),
 	`create_at` DATETIME,
 	`movie_or_series` BOOLEAN,
+	`budget` DECIMAL(12,6),
+	`grossing` DECIMAL(12,6),
+	`status` BOOLEAN DEFAULT true,
 	PRIMARY KEY(`id`)
 );
 
 CREATE TABLE `roles` (
 	`id` BINARY(16) NOT NULL UNIQUE,
 	`name` VARCHAR(255),
+	`status` BOOLEAN,
 	PRIMARY KEY(`id`)
 );
 
@@ -32,9 +35,12 @@ CREATE TABLE `accounts` (
 	`gender` BOOLEAN,
 	`email` VARCHAR(255) UNIQUE,
 	`google_id` VARCHAR(255),
-	`remember` VARCHAR(255),
 	`avatar` VARCHAR(255),
 	`level` SMALLINT DEFAULT 0,
+	`email_confirm` BOOLEAN,
+	`account_status_id` INT,
+	`account_online_id` INT,
+	`status` BOOLEAN,
 	PRIMARY KEY(`id`)
 );
 
@@ -43,18 +49,18 @@ CREATE TABLE `celebrity` (
 	`name` VARCHAR(255),
 	`birthday` DATE,
 	`country` VARCHAR(255),
-	`keywords` VARCHAR(255),
 	`bio` TEXT(65535),
 	`poster` VARCHAR(255),
-	`status` SMALLINT DEFAULT 1,
+	`status` BOOLEAN DEFAULT true,
 	PRIMARY KEY(`id`)
 );
 
-CREATE TABLE `movies_celebrity` (
+CREATE TABLE `casting` (
 	`id` BINARY(16) NOT NULL UNIQUE,
 	`movie_id` BINARY(16),
 	`celebrity_id` BINARY(16),
-	`job_id` BINARY(16),
+	`job_id` BINARY(16) NOT NULL,
+	`status` BOOLEAN,
 	PRIMARY KEY(`id`)
 );
 
@@ -64,9 +70,9 @@ CREATE TABLE `reviews` (
 	`star` INT,
 	`comment` TEXT(65535),
 	`created_at` DATETIME,
+	`account_id` BINARY(16) NOT NULL,
 	`movie_id` BINARY(16),
-	`account_id` BINARY(16),
-	`status` SMALLINT DEFAULT 1,
+	`status` BOOLEAN DEFAULT true,
 	PRIMARY KEY(`id`)
 );
 
@@ -74,7 +80,7 @@ CREATE TABLE `genres` (
 	`id` BINARY(16) NOT NULL UNIQUE,
 	`name` VARCHAR(255),
 	`slug` VARCHAR(255),
-	`status` INT DEFAULT 1,
+	`status` BOOLEAN DEFAULT true,
 	PRIMARY KEY(`id`)
 );
 
@@ -82,13 +88,15 @@ CREATE TABLE `images_celebrity` (
 	`id` BINARY(16) NOT NULL UNIQUE,
 	`link` VARCHAR(255),
 	`celebrity_id` BINARY(16),
+	`status` BOOLEAN,
 	PRIMARY KEY(`id`)
 );
 
 CREATE TABLE `movies_genres` (
 	`id` BINARY(16) NOT NULL UNIQUE,
-	`movie_id` BINARY(16),
-	`genres_id` BINARY(16),
+	`genres_id` BINARY(16) NOT NULL,
+	`movie_id` BINARY(16) NOT NULL,
+	`status` BOOLEAN,
 	PRIMARY KEY(`id`)
 );
 
@@ -97,15 +105,24 @@ CREATE TABLE `episodes` (
 	`name` VARCHAR(255),
 	`link` VARCHAR(255),
 	`description` TEXT(65535),
-	`movie_id` BINARY(16),
-	`status` SMALLINT DEFAULT 1,
+	`movie_id` BINARY(16) NOT NULL,
+	`duration` DATETIME,
+	`file_size` INT,
+	`license_production_id` BINARY(16),
+	`license_price` DECIMAL(12,6),
+	`license_start` DATETIME,
+	`license_end` DATETIME,
+	`tax` DECIMAL(12,6),
+	`status` BOOLEAN DEFAULT true,
 	PRIMARY KEY(`id`)
 );
 
 CREATE TABLE `favourites` (
 	`id` BINARY(16) NOT NULL UNIQUE,
-	`accounts_id` BINARY(16),
-	`movie_id` BINARY(16),
+	`movie_id` BINARY(16) NOT NULL,
+	`accounts_id` BINARY(16) NOT NULL,
+	`favourite_at` DATETIME,
+	`status` BOOLEAN,
 	PRIMARY KEY(`id`)
 );
 
@@ -115,33 +132,37 @@ CREATE TABLE `news` (
 	`created_at` DATETIME,
 	`tag` VARCHAR(255),
 	`description` TEXT(65535),
-	`status` SMALLINT DEFAULT 1,
+	`status` BOOLEAN DEFAULT true,
 	PRIMARY KEY(`id`)
 );
 
 CREATE TABLE `category` (
 	`id` BINARY(16) NOT NULL UNIQUE,
 	`name` VARCHAR(255),
+	`status` BOOLEAN,
 	PRIMARY KEY(`id`)
 );
 
 CREATE TABLE `categories_news` (
 	`id` BINARY(16) NOT NULL UNIQUE,
-	`category_id` BINARY(16),
-	`news_id` BINARY(16),
+	`category_id` BINARY(16) NOT NULL,
+	`news_id` BINARY(16) NOT NULL,
+	`status` BOOLEAN,
 	PRIMARY KEY(`id`)
 );
 
 CREATE TABLE `favorite_genres` (
 	`id` BINARY(16) NOT NULL UNIQUE,
-	`accounts_id` BINARY(16),
-	`genres_id` BINARY(16),
+	`accounts_id` BINARY(16) NOT NULL,
+	`genres_id` BINARY(16) NOT NULL,
+	`status` BOOLEAN,
 	PRIMARY KEY(`id`)
 );
 
 CREATE TABLE `job` (
 	`id` BINARY(16) NOT NULL UNIQUE,
 	`roles_name` VARCHAR(255),
+	`status` BOOLEAN,
 	PRIMARY KEY(`id`)
 );
 
@@ -149,45 +170,39 @@ CREATE TABLE `images_movies` (
 	`id` BINARY(16) NOT NULL UNIQUE,
 	`link` VARCHAR(255),
 	`movies_id` BINARY(16),
-	`status` SMALLINT DEFAULT 1,
+	`status` BOOLEAN DEFAULT true,
 	PRIMARY KEY(`id`)
 );
 
 
 CREATE INDEX `movies_id_index`
 ON `images_movies` (`movies_id`);
-CREATE TABLE `category_movie` (
-	`id` BINARY(16) NOT NULL UNIQUE,
-	`name` VARCHAR(255),
-	`slug` VARCHAR(255),
-	`status` SMALLINT DEFAULT 1,
-	PRIMARY KEY(`id`)
-);
-
 CREATE TABLE `helps` (
 	`id` INT NOT NULL AUTO_INCREMENT UNIQUE,
 	`question` TEXT(65535),
 	`answer` TEXT(65535),
-	`status` SMALLINT DEFAULT 1,
+	`status` BOOLEAN DEFAULT true,
 	PRIMARY KEY(`id`)
 );
 
 CREATE TABLE `payments` (
-	`id` INT NOT NULL AUTO_INCREMENT UNIQUE,
-	`account_id` BINARY(16),
-	`movie_id` BINARY(16),
+	`id` BINARY(16) NOT NULL UNIQUE,
+	`account_id` BINARY(16) NOT NULL UNIQUE,
 	`created_at` DATETIME,
-	`price` DECIMAL(12,4),
+	`total_price` DECIMAL(12,4),
 	`creditcard_type` VARCHAR(255),
 	`creditcard_number` VARCHAR(255),
-	`status` SMALLINT DEFAULT 1,
+	`email_sended` BOOLEAN,
+	`package_id` INT NOT NULL,
+	`status` BOOLEAN DEFAULT true,
 	PRIMARY KEY(`id`)
 );
 
 CREATE TABLE `account_role` (
 	`id` BINARY(16) NOT NULL UNIQUE,
-	`accounts_id` BINARY(16),
-	`role_id` BINARY(16),
+	`accounts_id` BINARY(16) NOT NULL,
+	`role_id` BINARY(16) NOT NULL,
+	`status` BOOLEAN,
 	PRIMARY KEY(`id`)
 );
 
@@ -195,16 +210,88 @@ CREATE TABLE `celebrity_job` (
 	`id` INT NOT NULL AUTO_INCREMENT UNIQUE,
 	`celebrity_id` BINARY(16),
 	`job_id` BINARY(16),
+	`status` BOOLEAN,
+	PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `watch_movie_status` (
+	`id` BINARY(16) NOT NULL UNIQUE,
+	`account_id` BINARY(16),
+	`movie_id` BINARY(16),
+	`watch_status_id` INT NOT NULL,
+	`view_status` BINARY(16),
+	`status` BOOLEAN,
+	PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `view_status` (
+	`id` BINARY(16) NOT NULL UNIQUE,
+	`watch_duration` DATETIME,
+	`date_stop_watch` DATETIME,
+	`status` BOOLEAN,
+	PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `package` (
+	`id` INT NOT NULL AUTO_INCREMENT UNIQUE,
+	`package_name` VARCHAR(255),
+	`price` DECIMAL(12,6),
+	`level` SMALLINT CHECK(4),
+	`description` TEXT(65535),
+	`status` BOOLEAN,
+	PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `watch_status` (
+	`id` INT NOT NULL AUTO_INCREMENT UNIQUE,
+	`name` VARCHAR(255),
+	`color` VARCHAR(255),
+	`status` BOOLEAN,
+	PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `account_status` (
+	`id` INT NOT NULL AUTO_INCREMENT UNIQUE,
+	`name` VARCHAR(255),
+	`change_at` DATETIME,
+	`color` VARCHAR(255),
+	`status` BOOLEAN,
+	PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `license_production` (
+	`id` BINARY(16) NOT NULL UNIQUE,
+	`license_name` VARCHAR(255),
+	`company_type	` VARCHAR(255),
+	`tax_number` VARCHAR(255),
+	`country` VARCHAR(255),
+	`status` BOOLEAN,
+	PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `account_online` (
+	`id` INT NOT NULL AUTO_INCREMENT UNIQUE,
+	`status_name` VARCHAR(255),
+	`online_offline_at` DATETIME,
+	`color` VARCHAR(255),
+	`status` BOOLEAN,
+	PRIMARY KEY(`id`)
+);
+
+CREATE TABLE ` distributors` (
+	`id` BINARY(16) NOT NULL UNIQUE,
+	`distributors_name` VARCHAR(255),
+	`company_type` VARCHAR(255),
+	`tax_number` VARCHAR(255),
+	`country` VARCHAR(255),
+	`status` BOOLEAN,
 	PRIMARY KEY(`id`)
 );
 
 ALTER TABLE `images_movies`
 ADD FOREIGN KEY(`movies_id`) REFERENCES `movies`(`id`)
 ON UPDATE CASCADE ON DELETE SET NULL;
-ALTER TABLE `movies`
-ADD FOREIGN KEY(`categories_movies_id`) REFERENCES `category_movie`(`id`)
-ON UPDATE CASCADE ON DELETE SET NULL;
-ALTER TABLE `movies_celebrity`
+ALTER TABLE `casting`
 ADD FOREIGN KEY(`movie_id`) REFERENCES `movies`(`id`)
 ON UPDATE CASCADE ON DELETE SET NULL;
 ALTER TABLE `favorite_genres`
@@ -232,13 +319,7 @@ ALTER TABLE `favourites`
 ADD FOREIGN KEY(`movie_id`) REFERENCES `movies`(`id`)
 ON UPDATE CASCADE ON DELETE SET NULL;
 ALTER TABLE `reviews`
-ADD FOREIGN KEY(`movie_id`) REFERENCES `movies`(`id`)
-ON UPDATE CASCADE ON DELETE SET NULL;
-ALTER TABLE `reviews`
 ADD FOREIGN KEY(`account_id`) REFERENCES `accounts`(`id`)
-ON UPDATE CASCADE ON DELETE SET NULL;
-ALTER TABLE `payments`
-ADD FOREIGN KEY(`movie_id`) REFERENCES `movies`(`id`)
 ON UPDATE CASCADE ON DELETE SET NULL;
 ALTER TABLE `payments`
 ADD FOREIGN KEY(`account_id`) REFERENCES `accounts`(`id`)
@@ -258,22 +339,42 @@ ON UPDATE CASCADE ON DELETE SET NULL;
 ALTER TABLE `celebrity_job`
 ADD FOREIGN KEY(`job_id`) REFERENCES `job`(`id`)
 ON UPDATE CASCADE ON DELETE SET NULL;
+ALTER TABLE `casting`
+ADD FOREIGN KEY(`celebrity_id`) REFERENCES `celebrity`(`id`)
+ON UPDATE CASCADE ON DELETE SET NULL;
+ALTER TABLE `watch_movie_status`
+ADD FOREIGN KEY(`account_id`) REFERENCES `accounts`(`id`)
+ON UPDATE CASCADE ON DELETE SET NULL;
+ALTER TABLE `watch_movie_status`
+ADD FOREIGN KEY(`movie_id`) REFERENCES `movies`(`id`)
+ON UPDATE CASCADE ON DELETE SET NULL;
 ALTER TABLE `images_celebrity`
 ADD FOREIGN KEY(`celebrity_id`) REFERENCES `celebrity`(`id`)
 ON UPDATE CASCADE ON DELETE SET NULL;
-ALTER TABLE `movies_celebrity`
-ADD FOREIGN KEY(`celebrity_id`) REFERENCES `celebrity`(`id`)
+ALTER TABLE `payments`
+ADD FOREIGN KEY(`package_id`) REFERENCES `package`(`id`)
 ON UPDATE CASCADE ON DELETE SET NULL;
-ALTER TABLE `movies_celebrity`
+ALTER TABLE `watch_movie_status`
+ADD FOREIGN KEY(`watch_status_id`) REFERENCES `watch_status`(`id`)
+ON UPDATE CASCADE ON DELETE SET NULL;
+ALTER TABLE `casting`
 ADD FOREIGN KEY(`job_id`) REFERENCES `job`(`id`)
 ON UPDATE CASCADE ON DELETE SET NULL;
-
-
---bỏ movie_id ở payments
-ALTER TABLE payments DROP FOREIGN KEY `payments_ibfk_1`;
-ALTER TABLE payments DROP COLUMN movie_id;
-
--- sửa account_id ở payments là unique do mỗi tài khoản chỉ cần trả tiền 1 lần là được(theo yêu cầu của nhóm trưởng)
-
-ALTER IGNORE TABLE `payments` ADD UNIQUE (`account_id`);
-
+ALTER TABLE `accounts`
+ADD FOREIGN KEY(`account_status_id`) REFERENCES `account_status`(`id`)
+ON UPDATE CASCADE ON DELETE SET NULL;
+ALTER TABLE `episodes`
+ADD FOREIGN KEY(`license_production_id`) REFERENCES `license_production`(`id`)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+ALTER TABLE `reviews`
+ADD FOREIGN KEY(`movie_id`) REFERENCES `movies`(`id`)
+ON UPDATE CASCADE ON DELETE SET NULL;
+ALTER TABLE `watch_movie_status`
+ADD FOREIGN KEY(`view_status`) REFERENCES `view_status`(`id`)
+ON UPDATE CASCADE ON DELETE SET NULL;
+ALTER TABLE `accounts`
+ADD FOREIGN KEY(`account_online_id`) REFERENCES `account_online`(`id`)
+ON UPDATE CASCADE ON DELETE SET NULL;
+ALTER TABLE `movies`
+ADD FOREIGN KEY(`distributors_id`) REFERENCES ` distributors`(`id`)
+ON UPDATE CASCADE ON DELETE SET NULL;
