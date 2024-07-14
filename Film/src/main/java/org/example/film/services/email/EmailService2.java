69744810fcfd6156fba9fe6cd6948a692ea6ef94
NCanhhieu <109597988+NCanhhieu@users.noreply.github.com>
2024-07-15 00:47:15 +0700

    google login otp

--- Film/src/main/java/org/example/film/services/email/EmailService2.java
+++ Film/src/main/java/org/example/film/services/email/EmailService2.java
@@ -1,5 +1,6 @@
 package org.example.film.services.email;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,10 +11,15 @@ public class EmailService2 {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private HttpServletRequest request;
     public void sendConfirmationEmail(String email, String confirmationToken ) {
         SimpleMailMessage message = new SimpleMailMessage();
-        String confirmationLink = "http://localhost:8080/v1/auth/confirm-email?token=" + confirmationToken;
+         // Get the current URL
+        String currentUrl = request.getRequestURL().toString();
 
+        // Construct the confirmation link using the current URL and the confirmation token
+        String confirmationLink = currentUrl.replace(request.getRequestURI(), "") + "/v1/auth/confirm-email?token=" + confirmationToken;
         message.setTo(email);
         message.setSubject("Confirm Your Email Address");
         message.setText("Please click the following link to confirm your email address:\n\n" + confirmationLink);
